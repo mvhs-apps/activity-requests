b24ec691f111154305cce37eee082f3fa4c68748
Arjun Patrawala <ajpat1234@gmail.com>
2018-11-06 16:19:59 -0800

    backend started

---
+++ backend/lib/routes.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const router = express.Router();
+
+const responses = require('./responses');
+
+
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const routes = require('./lib/routes');
+const responses = require('./lib/responses');
+
+const app = express();
+
+app.use(express.json());
+
+app.options('*', cors());
+app.use(cors({
+    origin(origin, callback) {
+        callback(null, true);
+    }
+}));
+
+app.use('/api', routes);
+
+app.all('*', (req, res) => {
+    res.json(responses.error(404));
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+});
