c268dc5eaded4c2d41d1662eed9335c739139d42
Arjun Patrawala <ajpat1234@gmail.com>
2018-11-10 23:28:57 -0800

    more work on form

--- frontend/src/views/FormPage.vue
+++ frontend/src/views/FormPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <span class="form-section-title">General Questions</span>
-        <div style="padding: 0 100px;">
+        <div class="input-group">
             <div class="input-area">
                 <div class="text-input">
                     <span class="input-title">Your Name</span>
@@ -23,14 +23,14 @@
             <div class="input-area">
                 <div class="text-input">
                     <span class="input-title">Your Email</span>
-                    <input type="text" v-model="form.general.student_email" placeholder="MVLA email">
+                    <input type="text" v-model.trim="form.general.student_email" placeholder="MVLA email">
                     <span class="feedback" style="color: #d50000;">Please enter a valid MVLA email</span>
                 </div>
             </div>
             <div class="input-area">
                 <div class="text-input">
                     <span class="input-title">Club Advisor Email</span>
-                    <input type="text" v-model="form.general.advisor_email" placeholder="MVLA email">
+                    <input type="text" v-model.trim="form.general.advisor_email" placeholder="MVLA email">
                     <span class="feedback" style="color: #d50000;">Please enter a valid MVLA email</span>
                 </div>
             </div>
@@ -46,7 +46,7 @@
                 <div class="text-input">
                     <span class="input-title" style="margin-bottom: 0;">Start date of event</span>
                     <span class="input-subtitle">This can be the day of the event or, if your event spans multiple days, the first day of the event</span>
-                    <input type="date" v-model="form.general.start_date" placeholder="mm/dd/yyyy">
+                    <input type="date" v-model.trim="form.general.start_date" placeholder="mm/dd/yyyy">
                     <span class="feedback" style="color: #d50000;">Please enter a valid MVLA email</span>
                 </div>
             </div>
@@ -83,11 +83,56 @@
             </div>
         </div>
 
-        <div v-if="form.general.event_on_campus === 'yes'">
-            <span class="form-section-title">Campus Questions</span>
+        <span v-show="form.general.event_on_campus === 'yes'" class="form-section-title">Campus Questions</span>
+        <div v-show="form.general.event_on_campus === 'yes'" class="input-group">
+            <div class="checkbox-input">
+                <span class="input-title">Please select desired facilities</span>
+                <div class="md-checkbox">
+                    <input type="checkbox" v-model="form.campus.gym" id="gym-checkbox">
+                    <label for="gym-checkbox">Gym/fields</label>
+                </div>
+                <div class="md-checkbox">
+                    <input type="checkbox" v-model="form.campus.ccc" id="ccc-checkbox">
+                    <label for="ccc-checkbox">College and Career Center</label>
+                </div>
+                <div class="md-checkbox">
+                    <input type="checkbox" v-model="form.campus.cafeteria" id="cafeteria-checkbox">
+                    <label for="cafeteria-checkbox">Cafeteria</label>
+                </div>
+                <div class="md-checkbox">
+                    <input type="checkbox" v-model="form.campus.library" id="library-checkbox">
+                    <label for="library-checkbox">Library</label>
+                </div>
+                <div class="md-checkbox">
+                    <input type="checkbox" v-model="form.campus.classroom" id="classroom-checkbox">
+                    <label for="classroom-checkbox">Classroom</label>
+                </div>
+                <div class="input-area checkbox-extension" v-show="form.campus.classroom">
+                    <div class="text-input">
+                        <span class="input-title">Which classrooms?</span>
+                        <div>
+                            <p class="hoverable"><i class="material-icons">help</i></p>
+                            <div>Enter specific details including, but not limited to, room numbers and times.</div>
+                        </div>
+                        <textarea class="text-input" @click="e => e.target.classList.add('expanded')" v-model="form.campus['classroom-extra-info']" placeholder="Write here"></textarea>
+                    </div>
+                </div>
+                <div class="md-checkbox">
+                    <input type="checkbox" v-model="form.campus.theater" id="theater-checkbox">
+                    <label for="theater-checkbox">Theater</label>
+                </div>
+                <div class="input-area checkbox-extension" v-show="form.campus.theater">
+                    <div class="text-input">
+                        <span class="input-title" style="margin-bottom: 0;">Please fill out this form for theater use</span>
+                        <span class="input-subtitle">Passoword is 'spartans'</span>
+                        <br>
+                        <a href="https://www.spartantheatermvhs.com/bookingmvla/" target="_blank">https://www.spartantheatermvhs.com/bookingmvla/</a>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div v-if="form.general.is_fundraiser === 'yes'">
+        <div v-show="form.general.is_fundraiser === 'yes'" class="input-group">
             <span class="form-section-title">Fundraiser Questions</span>
         </div>
     </div>
@@ -159,6 +204,17 @@
         transition: all 0.4s ease;
     }
 
+    .input-group {
+        padding: 0px 100px;
+    }
+
+    .checkbox-extension {
+        padding-left: 20px;
+        border-radius: 8px;
+        border: 1px solid rgba(0,0,0,.1);
+        box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
+    }
+
     .text-input > div, .select-input > div {
         display: inline-block;
     }
@@ -225,5 +281,67 @@
         .hoverable + div {
             width: 250px;
         }
+    }
+
+    /* checkbox-stuff */
+    .md-checkbox {
+        position: relative;
+        margin: 16px 0;
+        text-align: left;
+    }
+    .md-checkbox.md-checkbox-inline {
+        display: inline-block;
+    }
+    .md-checkbox label {
+        cursor: pointer;
+        font-weight: normal;
+        -moz-user-select: -moz-none;
+        -khtml-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+    .md-checkbox label:before, .md-checkbox label:after {
+        content: "";
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
+    .md-checkbox label:before {
+        width: 20px;
+        height: 20px;
+        background: #fff;
+        border: 2px solid rgba(0, 0, 0, 0.54);
+        border-radius: 2px;
+        cursor: pointer;
+        transition: background 0.3s;
+    }
+    .md-checkbox input[type="checkbox"] {
+        outline: 0;
+        margin-right: 10px;
+        visibility: hidden;
+    }
+    .md-checkbox input[type="checkbox"]:checked + label:before {
+        background: #337ab7;
+        border: none;
+    }
+    .md-checkbox input[type="checkbox"]:checked + label:after {
+        transform: rotate(-45deg);
+        top: 5px;
+        left: 4px;
+        width: 12px;
+        height: 6px;
+        border: 2px solid #fff;
+        border-top-style: none;
+        border-right-style: none;
+    }
+    .md-checkbox input[type="checkbox"]:disabled + label:before {
+        border-color: rgba(0, 0, 0, 0.26);
+    }
+    .md-checkbox input[type="checkbox"]:disabled:checked + label:before {
+        background: rgba(0, 0, 0, 0.26);
+    }
+    *, *:before, *:after {
+        box-sizing: border-box;
     }
 </style>
